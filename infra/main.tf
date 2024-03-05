1cd0adeb313d18c0927f9c5fbbe5b63d43cf9911
jamieastley <6836556+jamieastley@users.noreply.github.com>
2024-03-05 17:34:20 +1100

    configure variables

--- infra/main.tf
+++ infra/main.tf
@@ -8,11 +8,11 @@ terraform {
 }
 
 provider "cloudflare" {
-  api_token = "<YOUR_API_TOKEN>"
+#  api_token = <loaded via env var>
 }
 
 resource "cloudflare_r2_bucket" "cloudflare-bucket" {
-  account_id = "<YOUR_ACCOUNT_ID>"
-  name       = "my-tf-test-bucket"
-  location   = "apac"
+  account_id = var.account_id
+  name       = var.r2_bucket_name
+  location   = var.r2_bucket_location
 }
